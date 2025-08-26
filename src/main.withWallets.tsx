88d13cc86c5d3916128d07484d7aa599ee719b99
Br-iON <26982008+KaliStudent@users.noreply.github.com>
2025-08-26 01:28:58 -0500

    Update main.withWallets.tsx

--- src/main.withWallets.tsx
+++ src/main.withWallets.tsx
@@ -4,12 +4,12 @@ import { createRoot } from 'react-dom/client'
 import App from './pages/App.withWallets'
 import './styles/globals.css'
 import '@solana/wallet-adapter-react-ui/styles.css'
-import { GhillieWalletProvider } from './wallet/WalletProvider'
+import { WalletProvider } from './wallet/WalletProvider'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GhillieWalletProvider>
+    <WalletProvider>
       <App />
-    </GhillieWalletProvider>
+    </WalletProvider>
   </React.StrictMode>
 )
