9fb37481e62f537d70a959f036efba8a27fc8cc0
Br-iON <26982008+KaliStudent@users.noreply.github.com>
2025-08-26 01:33:37 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./pages/App.withWallets";    
-import "./styles/globals.css";
-import "@solana/wallet-adapter-react-ui/styles.css";
-import { WalletProvider } from "./wallet/WalletProvider";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './pages/App.withWallets'
+import './styles/globals.css'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import { WalletProvider } from './wallet/WalletProvider'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WalletProvider>
       <App />
     </WalletProvider>
   </React.StrictMode>
-);
+)
