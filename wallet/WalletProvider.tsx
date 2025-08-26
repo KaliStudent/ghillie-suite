e43f725dbe1e4e8db862e29e4d07dff349e15de0
Br-iON <26982008+KaliStudent@users.noreply.github.com>
2025-08-26 01:10:25 -0500

    Add files via upload

---
+++ wallet/WalletProvider.tsx
@@ -0,0 +1,29 @@
+
+import { FC, ReactNode, useMemo } from "react";
+import { ConnectionProvider, WalletProvider as BaseWalletProvider } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+export const GhillieWalletProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <BaseWalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </BaseWalletProvider>
+    </ConnectionProvider>
+  );
+};
